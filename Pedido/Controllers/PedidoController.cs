a4e5a3aa12195e7d4a6a1404b4d69b3fff601cbd
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-20 20:40:11 -0300

    Repositorio

---
+++ Pedido/Controllers/PedidoController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pedido
+{
+
+    [Route("api/[controller]")]
+    public class PedidoController : Controller
+    {
+
+        
+        
+    }
+}
---
+++ Pedido/Repositorio/RepositorioPedido.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Pedido
+{
+    public class RepositorioPedido
+    {
+        
+        public static List<Pedido> _listaPedido = new List<Pedido>{
+            new Pedido{ Id=01, NomeCliente="João",Email="joao@everis.com",
+                        Cpf="387497666777", ValorTotal=12345.78, DataPedido=DateTime.Now.ToLocalTime() },
+            
+        };
+
+    }
+}
